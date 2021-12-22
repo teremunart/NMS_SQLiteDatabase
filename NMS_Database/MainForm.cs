eeb9dfa16bc33f037bd68bd002a38b67b30cbf30
Teremun <rda@hhek.de>
2021-12-22 12:58:20 +0100

    Fix for the NullException for lists

--- NMS_Database/MainForm.cs
+++ NMS_Database/MainForm.cs
@@ -7,11 +7,10 @@ using System.Windows.Forms;
 // TODO: Add Moon & Planet count
 // TODO: Add Color Translator for Galaxy (might be useful for design)
 // TODO: Add all the other tabs
-// TODO: Handle NullException for not visible elements in lists
 
 namespace NMS_Database {
     public partial class MainForm : Form {
-        public PictureBox[] portalBar = new PictureBox[12];
+        private readonly PictureBox[] _portalBar = new PictureBox[12];
         
         public MainForm() {
             InitializeComponent();
@@ -24,18 +23,18 @@ namespace NMS_Database {
             int euclidTemp = Tab1GalaxyList.SelectedIndex;
             ListFiller("t1_system", $"SELECT DISTINCT SystemName FROM NMS_System, Galaxy WHERE NMS_System.GalaxyID={euclidTemp};");
             //Fill PictureBox Array
-            portalBar[0] = portal_1;
-            portalBar[1] = portal_2;
-            portalBar[2] = portal_3;
-            portalBar[3] = portal_4;
-            portalBar[4] = portal_5;
-            portalBar[5] = portal_6;
-            portalBar[6] = portal_7;
-            portalBar[7] = portal_8;
-            portalBar[8] = portal_9;
-            portalBar[9] = portal_10;
-            portalBar[10] = portal_11;
-            portalBar[11] = portal_12;
+            _portalBar[0] = portal_1;
+            _portalBar[1] = portal_2;
+            _portalBar[2] = portal_3;
+            _portalBar[3] = portal_4;
+            _portalBar[4] = portal_5;
+            _portalBar[5] = portal_6;
+            _portalBar[6] = portal_7;
+            _portalBar[7] = portal_8;
+            _portalBar[8] = portal_9;
+            _portalBar[9] = portal_10;
+            _portalBar[10] = portal_11;
+            _portalBar[11] = portal_12;
         }
         
         //FUNCTIONS
@@ -72,7 +71,6 @@ namespace NMS_Database {
 
             return reader;
         }
-        
 
         // EVENTS TAB1
         private void Tab1GalaxyList_SelectedIndexChanged(object sender, EventArgs e) {
@@ -80,80 +78,88 @@ namespace NMS_Database {
         }
 
         private void Tab1SystemList_SelectedIndexChanged(object sender, EventArgs e) {
-            Tab1SelectedSystemLabel.Text = Tab1SystemList.SelectedItem.ToString().ToUpper();
-            Tab1PlanetList.Items.Clear();
-
-            SQLiteDataReader reader = GetDatabase($"SELECT * FROM NMS_System WHERE SystemName=\"{Tab1SystemList.SelectedItem}\";");
-            SystemData data = new SystemData();
+            try {
+                Tab1SelectedSystemLabel.Text = Tab1SystemList.SelectedItem.ToString().ToUpper();
+                Tab1PlanetList.Items.Clear();
+                
+                SQLiteDataReader reader = GetDatabase($"SELECT * FROM NMS_System WHERE SystemName=\"{Tab1SystemList.SelectedItem}\";");
+                SystemData data = new SystemData();
             
-            if(reader != null)
-                while (reader.Read()) {
-                    data.SystemId    = reader.GetInt32(0);
-                    data.SystemName  = reader.GetString(1);
-                    data.GalaxyId    = reader.GetInt32(2);
-                    data.PlanetCount = reader.GetInt32(3);
-                    data.Species     = reader.GetString(4);
-                    data.Economy     = reader.GetString(5);
-                    data.Conflict    = reader.GetString(6);
-                    data.MoonCount   = reader.GetInt32(7);
-                    data.Coords      = reader.GetString(8);
+                if(reader != null)
+                    while (reader.Read()) {
+                        data.SystemId    = reader.GetInt32(0);
+                        data.SystemName  = reader.GetString(1);
+                        data.GalaxyId    = reader.GetInt32(2);
+                        data.PlanetCount = reader.GetInt32(3);
+                        data.Species     = reader.GetString(4);
+                        data.Economy     = reader.GetString(5);
+                        data.Conflict    = reader.GetString(6);
+                        data.MoonCount   = reader.GetInt32(7);
+                        data.Coords      = reader.GetString(8);
+                    }
+                reader?.Close();
+
+                // Set Species
+                Tab1ShowSpecies.BackgroundImage = data.Species.ToLower() switch {
+                    "gek"    => Properties.Resources.sys_gek,
+                    "vykeen" => Properties.Resources.sys_vykeen,
+                    "korvax" => Properties.Resources.sys_korvax,
+                    _        => Properties.Resources.sys_empty
+                };
+
+                // Set Glyps
+                Utils u = new Utils();
+                int counter = 0;
+                char[] glyps = new char[12];
+                string portalAddress = u.GalacticToPortal(data.Coords);
+                foreach (char g in portalAddress) {
+                    glyps[counter] = g;
+                    counter++;
                 }
-            reader?.Close();
-
-            // Set Species
-            Tab1ShowSpecies.BackgroundImage = data.Species.ToLower() switch {
-                "gek"    => Properties.Resources.sys_gek,
-                "vykeen" => Properties.Resources.sys_vykeen,
-                "korvax" => Properties.Resources.sys_korvax,
-                _        => Properties.Resources.sys_empty
-            };
-
-            // Set Glyps
-            Utils u = new Utils();
-            int counter = 0;
-            char[] glyps = new char[12];
-            string portalAddress = u.GalacticToPortal(data.Coords);
-            foreach (char g in portalAddress) {
-                glyps[counter] = g;
-                counter++;
-            }
-            u.ChangeGlyphs(portalBar, glyps);
+                u.ChangeGlyphs(_portalBar, glyps);
             
-            // Update Infos
-            ListFiller("t1_planet", $"SELECT DISTINCT PlanetName FROM Planet, NMS_System WHERE Planet.SystemID=NMS_System.SystemID AND SystemName=\"{Tab1SystemList.SelectedItem}\";");
+                // Update Infos
+                ListFiller("t1_planet", $"SELECT DISTINCT PlanetName FROM Planet, NMS_System WHERE Planet.SystemID=NMS_System.SystemID AND SystemName=\"{Tab1SystemList.SelectedItem}\";");
+            } catch (Exception ex) {
+                // ignored
+            }
         }
         
         private void Tab1PlanetList_SelectedIndexChanged(object sender, EventArgs e) {
-            SQLiteDataReader reader = GetDatabase($"SELECT * FROM Planet WHERE PlanetName=\"{Tab1PlanetList.SelectedItem}\";");
+            try {
+                SQLiteDataReader reader = GetDatabase($"SELECT * FROM Planet WHERE PlanetName=\"{Tab1PlanetList.SelectedItem}\";");
             
-            if(reader != null)
-                while (reader.Read()) {
-                    // Save Data in object
-                    PlanetaryData sP = new PlanetaryData {
-                        PlanetId = reader.GetInt32(0),
-                        PlanetName = reader.GetString(1),
-                        PlanetType = reader.GetString(2),
-                        PlanetDescription = reader.GetString(3),
-                        SystemId = reader.GetInt32(4),
-                        OreTypes = reader.GetString(5),
-                        PlanetImage = reader.GetString(6)
-                    };
+                if(reader != null)
+                    while (reader.Read()) {
+                        // Save Data in object
+                        PlanetaryData sP = new PlanetaryData {
+                            PlanetId = reader.GetInt32(0),
+                            PlanetName = reader.GetString(1),
+                            PlanetType = reader.GetString(2),
+                            PlanetDescription = reader.GetString(3),
+                            SystemId = reader.GetInt32(4),
+                            OreTypes = reader.GetString(5),
+                            PlanetImage = reader.GetString(6)
+                        };
 
-                    if (sP.PlanetImage != "about:blank") {
-                        Tab1ImageOfPlanet.Visible = true;
-                        try {
-                            Tab1ImageOfPlanet.ImageLocation = sP.PlanetImage;
-                        } catch (Exception exception) {
-                            // ignored
+                        if (sP.PlanetImage != "about:blank") {
+                            Tab1ImageOfPlanet.Visible = true;
+                            try {
+                                Tab1ImageOfPlanet.ImageLocation = sP.PlanetImage;
+                            } catch (Exception exception) {
+                                // ignored
+                            }
+                        } else {
+                            Tab1ImageOfPlanet.Visible = false;
                         }
-                    } else {
-                        Tab1ImageOfPlanet.Visible = false;
-                    }
 
-                    Tab1PlanetNameLabel.Text = sP.PlanetName;
-                    Tab1DescriptionLabel.Text = sP.PlanetDescription;
-                }
-            reader?.Close();
+                        Tab1PlanetNameLabel.Text = sP.PlanetName;
+                        Tab1DescriptionLabel.Text = sP.PlanetDescription;
+                    }
+                reader?.Close();
+            } catch (Exception ex) {
+              //nothing  
+            }
         }
     }
 }
